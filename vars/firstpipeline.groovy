6394292ef54b6208d35b3282261b126cdfbac5ce
nagasai <nagasaikonderu@gmail.com>
2025-11-05 12:06:56 +0530

    shared-pipeline

---
+++ vars/firstpipeline.groovy
@@ -0,0 +1,52 @@
+import com.nagasai.builds.Calculator
+
+def call(Map pipelineParams){
+   Calculator cal = new Calculator(this)
+
+
+
+pipeline{
+    agent any
+    environment {
+        APPLICATION_NAME = "${pipelineParams.appName}"
+    }
+    stages {
+
+        stage ('calculate'){
+            steps {
+                script {
+               echo "calling add method for reuasbility"
+               println cal.add(14,18);
+                }
+            }
+        }
+       stage('Build'){
+           steps {
+            echo "*******building the application for ${env.APPLICATION_NAME}*********"
+           }
+       }
+
+       stage ('Test'){
+         steps {
+            echo "***********application testing **************"
+         }
+       }
+
+       stage ('devDeploy'){
+        steps {
+            echo "******deployong to dev env ******"
+        }
+       }
+
+       stage ('testDeploy')
+       {
+        steps {
+            echo "******deployong to test env ******"
+        }
+       }
+    }
+}
+}
+
+
+
