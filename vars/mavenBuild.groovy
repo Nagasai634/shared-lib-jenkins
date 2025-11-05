cfc12d23206952c2652176d448d8e7ed861f40a4
Nagasaivardhan <nagasaikonderu@gmail.com>
2025-11-05 15:19:31 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -3,12 +3,8 @@
 def call(Map params = [:]) {
     String pom = params.get('pom', 'pom.xml')
     String goals = params.get('goals', 'clean package -DskipTests=true')
-<<<<<<< HEAD
-    String mavenToolName = params.get('mavenTool', 'Maven 3.6.3') // adjust to your configured tool name
-    String buildDir = params.get('buildDir', null) // optional
-=======
+
     String mavenToolName = params.get('maven', 'Maven 3.8.7') // adjust to your configured tool name
->>>>>>> d0dd456ab9b68523838093a5ad653dd612df3796
 
     echo "mavenBuild: pom=${pom}, goals='${goals}', buildDir='${buildDir ?: '.'}'"
 
